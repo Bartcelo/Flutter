4f888ace92cc5da26de842d79fe9c58f5ad5ed36
BartCelo <bart.celo@hotmail.com>
2018-11-11 14:15:08 -0200

    APK gerado

--- Conversor/conversor_moedas/lib/main.dart
+++ Conversor/conversor_moedas/lib/main.dart
@@ -11,7 +11,7 @@ Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
-
+// apk gerado
 void main() async {
   //print(await getData());
 
